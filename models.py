1dce68df963aedaf652d681737a9e1d899e978c8
Josefine Lantz <jo.lantz@icloud.com>
2025-12-23 10:14:18 +0100

    add tests for CRUD operations

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Float, Date 
-from sqlalchemy.orm import relationship 
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, Date
+from sqlalchemy.orm import relationship
 
-from database import Base 
+from database import Base
+
 
 class Player(Base):
     __tablename__ = "player"
@@ -15,8 +16,11 @@ class Player(Base):
 
     performances = relationship("Performance", back_populates="player")
 
+
     # Many-to-many relationship between Player and Team tables
-    teams = relationship("Team", secondary="team_player", back_populates="players")
+    teams = relationship("Team", secondary="team_player", 
+                         back_populates="players")    
+
 
 class Performance(Base):
     __tablename__ = "performance"
@@ -26,9 +30,10 @@ class Performance(Base):
     fantasy_points = Column(Float, nullable=False)
     last_changed_date = Column(Date, nullable=False)
 
-    player_id = Column(Integer, primary_key=True, index=True)
-    
+    player_id = Column(Integer, ForeignKey("player.player_id"))
+
     player = relationship("Player", back_populates="performances")
+
 
 class League(Base):
     __tablename__ = "league"
@@ -40,24 +45,26 @@ class League(Base):
 
     teams = relationship("Team", back_populates="league")
 
+
 class Team(Base):
     __tablename__ = "team"
 
     team_id = Column(Integer, primary_key=True, index=True)
     team_name = Column(String, nullable=False)
-    last_changed_date = Column(Date, nullable=False)
+    last_changed_date = Column(Date, nullable=False)    
 
-    league_id = Column(Integer, primary_key=True, index=True)
+    league_id = Column(Integer, ForeignKey("league.league_id"))
 
     league = relationship("League", back_populates="teams")
 
-    players = relationship("Player", secondary="team_player", back_populates="teams")
+    players = relationship("Player", secondary="team_player", 
+                           back_populates="teams")
 
 class TeamPlayer(Base):
     __tablename__ = "team_player"
 
-    team_id = Column(Integer, ForeignKey("team.team_id"),
+    team_id = Column(Integer, ForeignKey("team.team_id"), 
                      primary_key=True, index=True)
-    player_id = Column(Integer, ForeignKey("player.player_id"),
+    player_id = Column(Integer, ForeignKey("player.player_id"), 
                        primary_key=True, index=True)
-    last_changed_date = Column(Date, nullable=False)
+    last_changed_date = Column(Date, nullable=False)    
